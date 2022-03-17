8852a47c862d56d4019165d2f8ad8ceaf9ede2a8
Weslley Costa <weslleydasilvacostaa@gmail.com>
2022-03-17 15:10:32 -0300

    vamos ver dnv dnv dnv dnv

--- src/main/java/com/solinftec/stock/service/StockService.java
+++ src/main/java/com/solinftec/stock/service/StockService.java
@@ -2,7 +2,6 @@ package com.solinftec.stock.service;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import com.solinftec.stock.dto.StockDTO;
 import com.solinftec.stock.model.Stock;
@@ -26,7 +25,7 @@ public class StockService implements IStockService {
         return stockRepository.findAll()
                 .stream()
                 .map(StockDTO::new)
-                .collect(Collectors.toList());
+                .toList();
     }
 
     public List<StockDTO> gettAll(){
